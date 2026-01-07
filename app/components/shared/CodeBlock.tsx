333514da6b4185589335bd306079057ae61c703c
m4bulmagd <m4bulmagd@gmail.com>
2026-01-07 19:18:04 +0100

    update code/syntax highlighter

--- app/components/shared/CodeBlock.tsx
+++ app/components/shared/CodeBlock.tsx
@@ -1,9 +1,40 @@
-"use client";
-
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark, oneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import { useTheme } from "next-themes";
+import { Refractor, registerLanguage } from 'react-refractor'
 import Clipoboard from "./Clipoboard";
+
+// Load any languages you want to use from `refractor`
+import js from 'refractor/javascript'
+import sql from 'refractor/sql'
+import bash from 'refractor/bash'
+import markdown from 'refractor/markdown'
+import css from 'refractor/css'
+import scss from 'refractor/scss'
+import python from 'refractor/python'
+import html from 'refractor/markup'
+import yaml from 'refractor/yaml'
+import graphql from 'refractor/graphql'
+import json from 'refractor/json'
+import java from 'refractor/java'
+import ts from 'refractor/typescript'
+import tsx from 'refractor/tsx'
+import jsx from 'refractor/jsx'
+
+
+// Then register them
+registerLanguage(js)
+registerLanguage(sql)
+registerLanguage(bash)
+registerLanguage(markdown)
+registerLanguage(css)
+registerLanguage(scss)
+registerLanguage(python)
+registerLanguage(html)
+registerLanguage(yaml)
+registerLanguage(graphql)
+registerLanguage(json)
+registerLanguage(java)
+registerLanguage(ts)
+registerLanguage(tsx)
+registerLanguage(jsx)
 
 type codeTypes = {
   value: {
@@ -14,32 +45,17 @@ type codeTypes = {
 };
 
 export default function CodeBlock({ value }: codeTypes) {
-  const { theme } = useTheme();
   return (
     <div className="my-6">
-      <div className="flex items-center justify-between bg-zinc-50 dark:bg-[#141414] border dark:border-zinc-800 border-zinc-200 rounded-t-lg px-4 py-3">
-        {value.filename && <p className="text-sm">{value.filename}</p>}
+      <div className="flex items-center justify-between bg-zinc-50 dark:bg-[#141414] border dark:border-zinc-800 border-zinc-200 rounded-t-lg px-4 py-3 translate-y-2">
+        {value.filename && <p className="text-sm">{value.filename}.{value.language}</p>}
         <Clipoboard content={value.code} />
       </div>
-      <SyntaxHighlighter
+      <Refractor
         language={value.language ?? "jsx"}
-        style={theme === "dark" ? oneDark : oneLight}
-        customStyle={{
-          margin: 0,
-          borderRadius: "0 0 0.5rem 0.5rem",
-          borderLeft: "1px solid",
-          borderRight: "1px solid",
-          borderBottom: "1px solid",
-          borderColor: theme === "dark" ? "rgb(39 39 42)" : "rgb(228 228 231)",
-          fontSize: "0.875rem",
-          lineHeight: "1.25rem",
-        }}
-        showLineNumbers={false}
-        wrapLines={true}
-        wrapLongLines={true}
-      >
-        {value.code}
-      </SyntaxHighlighter>
+        value={value.code}
+        className="text-sm border-x border-b dark:border-zinc-800 border-zinc-200 rounded-b-lg tracking-normal"
+      />
     </div>
   );
 }
